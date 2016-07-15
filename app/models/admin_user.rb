620dcaeb99368c82f01ab2653d08088f59e5111b
greeninja <=>
2016-07-15 11:52:45 +0100

    remove admin as a denied user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,13 +8,13 @@ class AdminUser < ApplicationRecord
   scope :sorted, lambda { order("admin_users.last_name ASC").order("admin_users.first_name ASC") }
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/
-  FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary','root','admin']
+  FORBIDDEN_USERNAMES = ['littlebopeep','humptydumpty','marymary','root']
   validates_presence_of :first_name
   validates_length_of :first_name, :maximum => 25
   validates_presence_of :last_name
   validates_length_of :last_name, :maximum => 50
   validates_presence_of :username
-  validates_length_of :username, :within => 8..25
+  validates_length_of :username, :within => 5..25
   validates_uniqueness_of :username
   validates_presence_of :email
   validates_length_of :email, :maximum => 100
