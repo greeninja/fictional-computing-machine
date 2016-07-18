c6479b5490ad609858d57eff8e583ed6c62209c8
greeninja <=>
2016-07-18 11:39:34 +0100

    fixes #5 ordering user agents

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_many :ticks
     accepts_nested_attributes_for :rats, :ticks, allow_destroy: true, reject_if: :all_blank 
   accepts_nested_attributes_for :ticks, reject_if: :all_blank
-  scope :sorted, lambda { order("users.name DESC") }
+  scope :sorted, lambda { order("users.name ASC") }
 end
