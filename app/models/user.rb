a0e9cb4360ae07088f89b5645db7ed8ae9a103c0
greeninja <=>
2016-07-20 12:40:31 +0100

    new sortable lambda

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     accepts_nested_attributes_for :rats, :ticks, allow_destroy: true, reject_if: :all_blank 
   accepts_nested_attributes_for :ticks, reject_if: :all_blank
   scope :sorted, lambda { order("users.name ASC") }
+  scope :sorted_by_id, lambda { order("users.id ASC") }
 end
