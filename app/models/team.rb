6de616b495c9d5438406599e7bc0a1dcfa30df28
greeninja <=>
2016-07-20 20:27:17 +0100

    more work on team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,7 @@
 class Team < ApplicationRecord
-  has_many :users
+  
+  has_many :user_teams
+  has_many :users, :through => :user_teams
   
   scope :sorted, lambda { order("teams.id ASC") }
 
---
+++ app/models/user_team.rb
@@ -0,0 +1,4 @@
+class UserTeam < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :team
+end
