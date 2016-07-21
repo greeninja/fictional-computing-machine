529eadb3ddc2c3cd9fe750ae8fd4fc49efd5a6b7
greeninja <=>
2016-07-21 23:15:49 +0100

    fixes #8

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :rats, :ticks, allow_destroy: true, reject_if: :all_blank 
   accepts_nested_attributes_for :ticks, reject_if: :all_blank
-  scope :sorted, lambda { order("users.name ASC") }
-  scope :sorted_by_id, lambda { order("users.id ASC") }
+  scope :sorted, lambda { order("users.id ASC") }
+  # scope :sorted_by_id, lambda { order("users.id ASC") }
   scope :uniq_team_id, lambda { select('DISTINCT(team_id)') }
 end
