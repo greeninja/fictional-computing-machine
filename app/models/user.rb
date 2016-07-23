313483c103efe6eb09ba4233848de80d6ad04574
greeninja <=>
2016-07-23 19:04:38 +0100

    trailing whitespace - breaks rebase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_many :rats
   has_many :ticks
-    accepts_nested_attributes_for :rats, :ticks, allow_destroy: true, reject_if: :all_blank 
+
+  accepts_nested_attributes_for :rats, :ticks, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :ticks, reject_if: :all_blank
   scope :sorted, lambda { order("users.name ASC") }
   scope :sorted_by_id, lambda { order("users.id ASC") }
