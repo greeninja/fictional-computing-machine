e91b5098e7152c7de933059b380e6fe23ae9eb5d
greeninja <=>
2016-07-23 18:23:01 +0100

    fixes #13

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,8 @@
 class Team < ApplicationRecord
   
   has_many :users
+  has_many :ticks, through: :users
+  has_many :rats, through: :users
   scope :sorted, lambda { order("teams.id ASC") }
 
 end
