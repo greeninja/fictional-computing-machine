084ad48a4fcded47562b0890a4c215a868b99a0d
greeninja <=>
2016-07-23 19:05:53 +0100

    trailing whitespace error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :ticks
   belongs_to :team
 
-  accepts_nested_attributes_for :rats, :ticks, allow_destroy: true, reject_if: :all_blank 
+  accepts_nested_attributes_for :rats, :ticks, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :ticks, reject_if: :all_blank
   scope :sorted, lambda { order("users.customid ASC") }
   scope :uniq_team_id, lambda { select('DISTINCT(team_id)') }
