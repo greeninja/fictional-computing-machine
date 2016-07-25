080c08ce7874e5cb5fefefb10fd80af63f72169f
greeninja <=>
2016-07-25 13:19:19 +0100

    fixes #16 not backward compatible. Will work with what is currently being used though

--- db/migrate/20160725094148_customid_to_big_int.rb
+++ db/migrate/20160725094148_customid_to_big_int.rb
@@ -1,5 +1,8 @@
 class CustomidToBigInt < ActiveRecord::Migration[5.0]
-  def change
-    change_column("users", "customid", :integer, :limit => 8, :default => 0)
+  def up
+    change_column("users", "customid", :integer, :limit => 8, :default => :null, :null => true)
+  end
+  def down
+    change_column("users", "customid", :string)
   end
 end
