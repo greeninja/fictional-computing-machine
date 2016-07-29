3f338c89ea0f8c705bb3ac80bbca8c603132dd86
greeninja <=>
2016-07-29 19:55:13 +0100

    add confirmed logged in

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,6 @@
 class SettingsController < ApplicationController
   before_action :set_setting, only: [:show, :edit, :update, :destroy]
+  before_action :confirm_logged_in
 
   def index
     @settings = Setting.sorted
