abebcac019340ec7937e29b30391216f5def74d8
greeninja <=>
2016-07-31 14:43:26 +0100

    messed up private method

--- app/controllers/rats_controller.rb
+++ app/controllers/rats_controller.rb
@@ -60,7 +60,7 @@ class RatsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_rat
-      @rat_types = Rat.find(params[:id])
+      @rat = Rat.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
