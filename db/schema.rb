5d87f7ca08875316a65d62e3c8cabea48133cc65
greeninja <=>
2016-07-31 14:09:07 +0100

    because of migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729143853) do
+ActiveRecord::Schema.define(version: 20160731125605) do
 
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "first_name",      limit: 25
@@ -87,9 +87,8 @@ ActiveRecord::Schema.define(version: 20160729143853) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
     t.string   "last_name"
-    t.string   "manual_team"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "team_id"
     t.bigint   "customid"
     t.index ["team_id"], name: "index_users_on_team_id", using: :btree
