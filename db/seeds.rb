a6174162dd3bef99700e412d89d1c9fbe04e1210
greeninja <nick@greeninja.co.uk>
2016-08-20 15:50:55 +0100

    change seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Seed an Admin user with password - Admin
 #
-defaultuser = User.create :first_name => "Admin", :last_name => "Admin", :username => "admin", :password => "admin", :email => "admin@example.com", :role => "admin"
+user = User.create :first_name => "Admin", :last_name => "Admin", :username => "admin", :password => "admin", :email => "admin@example.com", :role => "admin"
 
 ratsetting = Setting.create :name => "tick_types", :description => "This is where we define which Ticks we want to record in the user section of RaTs. These will also be displayed in the per-team graphs", :enabled => "0"
 
