d8a7cf6fc61d8a0aa0bff36d647f4308dd5f40f3
greeninja <nick@greeninja.co.uk>
2016-08-20 11:45:54 +0100

    remove TL ability to delete

--- app/policies/rat_policy.rb
+++ app/policies/rat_policy.rb
@@ -34,8 +34,7 @@ class RatPolicy < ApplicationPolicy
 
   def destroy?
     @current_user.admin? or
-    @current_user.junior_admin? or
-    @current_user.team_leader?
+    @current_user.junior_admin?
   end
 
 end
