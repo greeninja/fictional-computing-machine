f8dffaac67a4bc288dd9936b12f5c7bf3b62d9b3
greeninja <nick@greeninja.co.uk>
2016-09-11 12:57:19 +0100

    views working ok for qa - starting on forms

--- app/controllers/qas_controller.rb
+++ app/controllers/qas_controller.rb
@@ -2,31 +2,50 @@ class QasController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :get_setting, only: [:show]
-
+  before_action :set_dates
 
   def index
     @users = Agent.sorted
     @teams = Agent.uniq_team_id.where.not(:team_id => nil)
     @teamcount = Team.count
-    @date_from = parsed_date(params[:date_from], Date.today.beginning_of_month)
-    @date_to = parsed_date(params[:date_to], Date.today.end_of_month)
   end
 
   def show
-    @date_from = parsed_date(params[:date_from], Date.today.beginning_of_week)
-    @date_to = parsed_date(params[:date_to], Date.today.next_week)
-    @qa_settings = QaSetting.where("qa_settings.team_id = #{@user.team_id}").or(QaSetting.where("qa_settings.team_id is null")).sorted
+  end
+
+  def create
+    @qa = Qa.new(qa_params)
+    if @qa.save
+    # If save succeeds, redirect to the index action
+      flash[:notice] = "Qa created successfully"
+      redirect_to(:action => 'show', :id => @user.id )
+    else
+    # If save fails, redisplay the form so user can fix problems
+      render 'new', user: @user
+    end
   end
 
 
 
   private
+
   def set_user
     @user = Agent.find(params[:id])
   end
 
   def get_setting
     @setting = Setting.where(:name => "qa_settings")
+    @qa_settings = QaSetting.where("qa_settings.team_id = #{@user.team_id}").or(QaSetting.where("qa_settings.team_id is null")).sorted
+  end
+
+  def set_dates
+    @date_from = parsed_date(params[:date_from], Date.today.beginning_of_month)
+    @date_to = parsed_date(params[:date_to], Date.today.end_of_month)
+  end
+
+  def qa_params
+    params.require(:qa).permit()
   end
 
 end
+
--- app/models/qa.rb
+++ app/models/qa.rb
@@ -2,6 +2,19 @@ class Qa < ApplicationRecord
   has_many :settings
   belongs_to :ticket
 
+  before_create :add_ticket
+
   # has_paper_trail
+
+  def add_ticket
+    new_ticket = Ticket.new(params[:ticket_reference, :date, :agent_id, :score, :notes])
+
+    unless new_ticket.save
+      self.errors.add(:base, "Couldn't create ticker.")
+      return false
+    end
+
+    self.ticket_id = new_ticket.id
+  end
 
 end
