a44f22c9bc5b5442bb1a1e9170551b516de7f464
greeninja <nick@greeninja.co.uk>
2016-09-17 22:44:46 +0100

    remove papertrail from tickets model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,7 +4,7 @@ class Ticket < ApplicationRecord
   has_many :qas
   has_many :mets
 
-  has_paper_trail
+  # has_paper_trail
   accepts_nested_attributes_for :qas, allow_destroy: true, reject_if: :all_blank
 
   validates_presence_of :ticket_reference, :date
