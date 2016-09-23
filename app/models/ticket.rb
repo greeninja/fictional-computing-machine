d77453e943f6eed5404d2caa5634f93f7ad7f163
greeninja <nick@greeninja.co.uk>
2016-09-23 21:44:58 +0100

    stop same ticket being added to same user

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -8,5 +8,6 @@ class Ticket < ApplicationRecord
   accepts_nested_attributes_for :qas, allow_destroy: true, reject_if: :all_blank
 
   validates_presence_of :ticket_reference, :date
+  validates_uniqueness_of :ticket_reference, :scope => :agent_id
 
 end
