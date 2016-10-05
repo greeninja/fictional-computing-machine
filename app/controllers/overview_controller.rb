c780111949b8e1d57f45b4463ec9e6052a323cf9
greeninja <nick@greeninja.co.uk>
2016-10-05 22:34:17 +0100

    still working through agent views. Only QA left

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,6 +1,7 @@
 class OverviewController < ApplicationController
 
   before_action :set_dates
+  before_action :set_qa_dates
   before_action :confirm_logged_in
 
   def index
@@ -11,6 +12,7 @@ class OverviewController < ApplicationController
     if @current_user.agent_id?
       @agent = Agent.find(@current_user.agent_id)
       @team = Team.find(@agent.team_id)
+      @general_settings = QaGeneralSetting.where("qa_general_settings.disabled = false and qa_general_settings.team_id = #{ @team.id }").or(QaGeneralSetting.where("qa_general_settings.disabled = false and qa_general_settings.team_id = #{ @team.id }"))
       @get_team_crosses = @team.crosses.where('crosses.created_at BETWEEN ? AND ?', @date_from, @date_to)
       if @get_team_crosses.nil?
         @team_crosses = 0
@@ -31,6 +33,11 @@ class OverviewController < ApplicationController
   def set_dates
     @date_from = parsed_date(params[:date_from], Date.today.beginning_of_month)
     @date_to = parsed_date(params[:date_to], Date.today.end_of_month)
+  end
+
+  def set_qa_dates
+    @qa_date_from = parsed_date(params[:qa_date_from], 3.months.ago.beginning_of_month)
+    @qa_date_to = parsed_date(params[:qa_date_to], 1.month.ago.end_of_month.to_date)
   end
 
 end
--- app/helpers/overview_helper.rb
+++ app/helpers/overview_helper.rb
@@ -1,5 +1,7 @@
 module OverviewHelper
 
+########## Ticks and Cross Helpers
+
   def get_cross_percentages(agent)
     get_crosses = agent.crosses.where('crosses.created_at BETWEEN ? AND ?', @date_from, @date_to)
     if get_crosses.nil?
@@ -38,6 +40,52 @@ module OverviewHelper
       @tpercent = full_percent.nil? ? "0" : full_percent.round
     end
     @tnumber_of = count.nil? ? "0" : count.round
+  end
+
+########## QA Helpers
+
+  def ov_qa_average(user)
+    tickets = user.tickets.where('tickets.date BETWEEN ? AND ?', @qa_date_from, @qa_date_to)
+    total = tickets.sum(:score)
+    result = total.to_f / tickets.count
+    result.nan? ? "0" : result.round
+  end
+
+  def ov_qa_team_average(team)
+    tickets = team.tickets.where('tickets.date BETWEEN ? AND ?', @qa_date_from, @qa_date_to)
+    total = tickets.sum(:score)
+    result = total.to_f / tickets.count
+    result.nan? ? "0" : result.round
+  end
+
+  def ov_badge_it(score)
+    unless @general_settings.empty?
+      unless @general_settings.where(:name => "year_view_yellow_badge").empty?
+        yellow_score = @general_settings.where(:name => "year_view_yellow_badge").first
+      end
+      unless @general_settings.where(:name => "year_view_green_badge").empty?
+        green_score = @general_settings.where(:name => "year_view_green_badge").first
+      end
+      unless @general_settings.where(:name => "year_view_red_badge").empty?
+        red_score = @general_settings.where(:name => "year_view_red_badge").first
+      end
+
+      if score.to_f >= green_score.value.to_f
+        badge = "success"
+      elsif score.to_f >= yellow_score.value.to_f and score < green_score.value.to_f
+        badge = "warning"
+      elsif score.to_f <= red_score.value.to_f
+        badge = "danger"
+      else
+        badge = "default"
+      end
+
+      result = "<div class=\"progress-bar progress-bar-#{ badge }\" role=\"progressbar\" aria-valuenow=\"#{ score }\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"min-width: 2em; width: #{ score }%;\">"
+      result.html_safe
+
+    else
+      score
+    end
   end
 
 end
