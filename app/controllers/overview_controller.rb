72eb9e0f7f1c2483d1b331845e7db19bc429d046
greeninja <nick@greeninja.co.uk>
2016-10-05 17:56:40 +0100

    improving user view

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,12 +1,36 @@
 class OverviewController < ApplicationController
 
+  before_action :set_dates
   before_action :confirm_logged_in
 
   def index
     @agents = Agent.count
-    @date_from = parsed_date(params[:date_from], Date.today.beginning_of_week)
-    @date_to = parsed_date(params[:date_to], Date.today.next_week)
     # @crosses = Agent.crosses.count
     # @ticks = Agent.ticks.count
+
+    if @current_user.agent_id?
+      @agent = Agent.find(@current_user.agent_id)
+      @team = Team.find(@agent.team_id)
+      @get_team_crosses = @team.crosses.where('crosses.created_at BETWEEN ? AND ?', @date_from, @date_to)
+      if @get_team_crosses.nil?
+        @team_crosses = 0
+      else
+        @team_crosses = @get_team_crosses.count
+      end
+      @get_team_ticks = @team.ticks.where('ticks.created_at BETWEEN ? AND ?', @date_from, @date_to)
+      if @get_team_ticks.nil?
+        @team_ticks = 0
+      else
+        @team_ticks = @get_team_ticks.count
+      end
+    end
   end
+
+  private
+
+  def set_dates
+    @date_from = parsed_date(params[:date_from], Date.today.beginning_of_month)
+    @date_to = parsed_date(params[:date_to], Date.today.end_of_month)
+  end
+
 end
--- app/helpers/overview_helper.rb
+++ app/helpers/overview_helper.rb
@@ -1,2 +1,43 @@
 module OverviewHelper
+
+  def get_cross_percentages(agent)
+    get_crosses = agent.crosses.where('crosses.created_at BETWEEN ? AND ?', @date_from, @date_to)
+    if get_crosses.nil?
+      count = 0
+    else
+      count = get_crosses.count.to_f
+    end
+    if @team_crosses.nil?
+      full_percent = 0
+    else
+      full_percent = count / @team_crosses * 100
+    end
+    if full_percent.nan?
+      @cpercent = 0
+    else
+      @cpercent = full_percent.nil? ? "0" : full_percent.round
+    end
+    @cnumber_of = count.nil? ? "0" : count.round
+  end
+
+  def get_tick_percentages(agent)
+    get_ticks = agent.ticks.where('ticks.created_at BETWEEN ? AND ?', @date_from, @date_to)
+    if get_ticks.nil?
+      count = 0
+    else
+      count = get_ticks.count.to_f
+    end
+    if @team_ticks.nil?
+      full_percent = 0
+    else
+      full_percent = count / @team_ticks * 100
+    end
+    if full_percent.nan?
+      @tpercent = 0
+    else
+      @tpercent = full_percent.nil? ? "0" : full_percent.round
+    end
+    @tnumber_of = count.nil? ? "0" : count.round
+  end
+
 end
